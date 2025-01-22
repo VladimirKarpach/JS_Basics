1da345fc48c2873cb70a05886f725042d0842407
Uladzimir Karpach <vladimir.karpach@gmail.com>
2025-01-22 12:46:09 +0100

    First commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+"use strict";
+
+//alert('Hello!');
+
+//let result = confirm('Are you here?');
+//console.log(result);
+
+const answers = [];
+
+answers[0] = prompt('Your First Name', '');
+answers[1] = prompt('Your Last Name', '');
+answers[2] = prompt('Your Age', '');
+
+document.write(answers);
+
